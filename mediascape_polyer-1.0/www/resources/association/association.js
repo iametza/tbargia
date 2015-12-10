e6773a2fa900bb003bb60cc89c0839e4b6fa1823
Iker <iker@iametza.com>
2015-12-10 16:32:46 +0100

    associationpanel kendu eta partekatzeko ikonoa menuan sartu

--- mediascape_polyer-1.0/www/resources/association/association.js
+++ mediascape_polyer-1.0/www/resources/association/association.js
@@ -93,6 +93,83 @@ define( ["jquery","qrcode"], function($) {
         });
 			return p1;
     };
+	
+	this.createQRcodeRaw = function(_url,element,_width,_height,_type,marginLeft,marginTop){
+			console.log('createQRcode');
+			var p1= new Promise(
+				function(resolve,reject){
+					var deferred = $.Deferred();
+					var width=_width || "160";
+					var height=_height || "160";
+					var type=_type;
+					var url=_url;
+					var username="ionalberdi"; // bit.ly username
+					var key="R_978251cf3ed04317a147fde5a237b13c";
+					var promise = $.ajax({
+						url:"http://api.bit.ly/v3/shorten",
+						data:{
+							longUrl:url,
+							apiKey:key,
+							login:username
+						},
+						dataType:"jsonp"
+					})
+					.done(function(data, textStatus, jqXHR) {
+						console.log(data);
+						var shortedUrl = data.data.url;
+						var asociationElement=element;
+						var associationCode = document.createElement("div");
+						associationCode.id=type+"code";
+						var hElement = document.createElement("h2");
+						hElement.innerHTML=type;
+						associationCode.appendChild(hElement);
+						/*var title=document.createElement('div');
+              			title.style.color='white';
+              			title.innerHTML='Scan for another device';
+              			title.style.fontFamily='Nunito, arial, verdana';
+              			title.style.fontSize='50px';
+              			title.style.textAlign='center';
+              			title.style.marginTop='50px';
+              			associationCode.appendChild(title);*/
+						var qrElement = document.createElement("div");
+						qrElement.id=type+"qrcode";
+						/*
+						qrElement.style.width=width+"px";
+						qrElement.style.height=height+"px";
+						qrElement.style.marginLeft=marginLeft+"px";
+						qrElement.style.marginTop=marginTop+"px";
+						qrElement.style.border="4px solid grey";
+						qrElement.style.textAlign="center";*/
+						associationCode.style.transition="width 1.5s";
+						associationCode.appendChild(qrElement);
+						associationCode.appendChild(document.createElement("br"));
+						var urlElement = document.createElement("div");
+						urlElement.id="url";
+						urlElement.innerHTML=shortedUrl;
+						urlElement.style.height="20px";
+						urlElement.style.fontSize='30px';
+						urlElement.style.textAlign='center';
+						//urlElement.style.marginLeft=marginLeft+"px";
+						//urlElement.style.marginTop=marginTop+"px";
+						urlElement.style.color='white';
+						associationCode.appendChild(urlElement);
+						asociationElement.appendChild(associationCode);
+						var qrcode = new QRCode(type+"qrcode", {
+						    text: shortedUrl,
+						    width:width,
+						    height:height,
+						    colorDark : "#000000",
+						    colorLight : "#ffffff",
+						    correctLevel : QRCode.CorrectLevel.H
+						});
+						resolve(shortedUrl);
+					});
+
+        });
+			return p1;
+    };
+	
+	
 		this.getAssocatioUrl = function (){
 			return this.associationUrl;
 		}
